46bccd55d699885a2214877c07902e92c86becca
DHSYongJun <lim.yongjun@dhs.sg>
2020-11-19 17:09:04 +0800

    added logo citation

--- footer.inc.php
+++ footer.inc.php
@@ -9,5 +9,13 @@
 
 <footer class="container">
     <p>Copyright &copy; 2020 MovieReview Pte. Ltd.</p>
+    <div>Logo made by 
+        <a href="https://www.flaticon.com/authors/photo3idea-studio" 
+           title="photo3idea_studio">photo3idea_studio
+        </a> from 
+        <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+        </a>
+    </div>
 </footer>
 
