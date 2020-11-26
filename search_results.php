d454cacf84326c08cd2f2e242fefa6c334577fb0
DHSYongJun <lim.yongjun@dhs.sg>
2020-11-26 18:17:20 +0800

    better search

--- search_results.php
+++ search_results.php
@@ -1,6 +1,7 @@
 <?php
 
-$movieTitle = $genre = $actors = $releaseDate = $poster_portrait = $search_input = $errorMsg = "";
+$movieTitleArr = $genreArr = $actorsArr = $releaseDateArr = $poster_portraitArr = array();
+$search_input = $errorMsg = "";
 $success = true;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
@@ -13,7 +14,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     else
     {
         $search_input = sanitize_input($_POST["search_input"]);
-        echo $search_input;
         getSearchResults();
     }
 }
@@ -28,7 +28,7 @@ else
 //Helper function to fetch movie data.
 function getSearchResults()
 {
-    global $movieTitle, $genre, $actors, $releaseDate, $poster_landscape, $search_input;
+    global $movieTitleArr, $genreArr, $actorsArr, $releaseDateArr, $poster_portraitArr, $search_input;
 
     // Create database connection.
     $config = parse_ini_file('../../private/db-config.ini');
@@ -47,13 +47,15 @@ function getSearchResults()
         // lowercase search input
         $search_input = strtolower($search_input);
         // split input by space into array 
-        $array_of_words = $search_input;
-        echo $array_of_words;
+        $array_of_words = explode(" ", $search_input);
         $stmt = $conn->prepare("SELECT * FROM movies
-                                WHERE (LOWER(movieTitle) LIKE '" . $array_of_words . "%')");
-        
+                                WHERE (LOWER(movieTitle) LIKE '%" . $array_of_words[0] . "%')
+                                OR (LOWER(movieTitle) LIKE '%" . $array_of_words[1] . "%')
+                                OR (LOWER(movieTitle) LIKE '%" . $array_of_words[2] . "%')");
+
         $stmt->execute();
         $result = $stmt->get_result();
+
         if ($result->num_rows < 1)
         {   
             $errorMsg = "Movie not found";
@@ -67,7 +69,11 @@ function getSearchResults()
         $conn->close();
         
         while ($row = $result->fetch_assoc()) {
-            echo $row['movieTitle'];
+            array_push($movieTitleArr, $row['movieTitle']);
+            array_push($genreArr, $row['genre']);
+            array_push($actorsArr, $row['actors']);
+            array_push($releaseDateArr, $row['$releaseDate']);
+            array_push($poster_portraitArr, $row['$poster_portrait']);
         }
     }
 }
@@ -103,44 +109,31 @@ function sanitize_input($data)
                 <h1>Search Results</h1>
                 
                 <?php
-                if ($success) {
-                    echo $result;
-                    while ($row = $result->fetch_assoc()) {
+                for ($index = 0; $index < sizeof($movieTitleArr); $index++) {
                 ?>
-                
-                
                 <div class="row">
                     <div class="review-block">
                         <hr/>
                         <div class="row">
                             <div class="col-3">
-                                <img class="mini-movie-poster" src="<?=$row['poster_landscape']?>" alt="Movie Poster">
+                                <img class="mini-movie-poster" src="data:image/jpeg;base64,<?=chunk_split(base64_encode($poster_portraitArr[$index]))?>" alt="Movie Poster">
 
                             </div>
                             <div class="col-9 mt-4">
                                 <div id="star-rating">⭐⭐⭐⭐⭐</div>
-                                <h5><?=$row['movieTitle']?></h5>
-                                <h6 class="small text-muted">Release date: <?=$row['releaseDate']?></h6>
+                                <h5><?=$movieTitleArr[$index]?></h5>
+                                <h6 class="small text-muted">Release date: <?=$releaseDateArr[$index]?></h6>
                                 
-                                <h6> Movie Genre: <?=$row['genre']?> </h6>
+                                <h6> Movie Genre: <?=$genreArr[$index]?> </h6>
                                 <p>
-                                    Cast: <?=$row['actors']?>
+                                    Cast: <?=$actorsArr[$index]?>
                                 </p>
                             </div>
                         </div>
                         <hr/>
                     </div>
                 </div>
-                
                 <?php
-                    }
-                }
-                else 
-                {
-                    echo "<h1 class='display-4'>Oops!</h1>";
-                    echo "<h3>The following input errors were detected:</h3>";
-                    echo "<p class='text-secondary'>" . $errorMsg . "</p>";
-                    echo '<a class="btn btn-danger mb-3" href="index.php" role="button">Return to Home page</a>';
                 }
                 ?>
             </section>
