b2f0a6f10a807457c4f7fe91b506dbf74e39ff57
huiToT <hui61239@hotmail.com>
2020-11-28 16:18:19 +0800

    remove unusable css

--- head.inc.php
+++ head.inc.php
@@ -52,16 +52,16 @@
 
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
-<link rel="stylesheet" href="css/bootstrap.min.css">
+<!--<link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="css/mdb.min.css">
-<!-- Plugin file -->
+ Plugin file 
 <link rel="stylesheet" href="./css/addons/datatables.min.css">
 <link rel="stylesheet" href="css/style.css">
 
-<!-----JS----->
+---JS---
 <script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript" src="js/popper.min.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/mdb.min.js"></script>
-<!-- Plugin file -->
-<script src="./js/addons/datatables.min.js"></script>
+ Plugin file 
+<script src="./js/addons/datatables.min.js"></script>-->
