87d99c5ccb5bd1a5e60504112184a016fa1b9588
Lee Zhan Hong <zlee820@gmail.com>
2020-11-30 00:02:02 +0800

    Edited footer

--- footer.inc.php
+++ footer.inc.php
@@ -1,17 +1,13 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 ?>
 <button id="ToTopButton" title="Go back to Top">&#9650;</button>
-<footer class="container">
+<footer>
     <p>Copyright &copy; 2020 Popcorn Pte. Ltd.</p>
     <div>Logo made by 
-        <a href="https://www.flaticon.com/authors/photo3idea-studio" 
-           title="photo3idea_studio">photo3idea_studio
+        <a href="https://www.flaticon.com/authors/photo3idea-studio" title="photo3idea_studio">
+            photo3idea_studio
         </a> from 
         <a href="https://www.flaticon.com/" title="Flaticon">
             www.flaticon.com
