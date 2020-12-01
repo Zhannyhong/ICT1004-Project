e141f85883751ec12095f2cc46f0275dd876adb3
Lee Zhan Hong <zlee820@gmail.com>
2020-12-01 11:44:34 +0800

    Minor edits

--- about_us.php
+++ about_us.php
@@ -22,7 +22,7 @@ session_start();
         ?>
         <header class="jumbotron text-center">
             <h1 class="display-4">About Us</h1>
-            <span class="subhead">OUR MISSION</span>
+            <span class="subhead">WHAT WE DO</span>
             <p class="lead">
                 We are the world's most trusted recommendation resource for quality entertainment. At Popcorn,
                 you can find the stuff you love, or share your opinions with other avid movie watchers. If you are just
