9ad64363806f5a793f5a4fb832807b0e2432fbe2
DHSYongJun <lim.yongjun@dhs.sg>
2020-12-01 20:28:15 +0800

    added context specific links after leaving/updating review

--- movie_template.php
+++ movie_template.php
@@ -265,6 +265,8 @@ function fetchMovieData()
                 <div class="review">
                     <div>
                         <?php
+                        echo var_dump($_SESSION);
+                        $_SESSION['current_location'] = 'movie_template.php';
                         for ($index = 0; $index < sizeof($reviewRatingArr); $index++)
                         {
                         ?>
