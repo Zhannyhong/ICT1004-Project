92d5ad10441642d99c4508a19a368a1425c075f3
DHSYongJun <lim.yongjun@dhs.sg>
2020-12-01 20:55:34 +0800

    fixed movieID not getting passed when editing reviews

--- process_review.php
+++ process_review.php
@@ -104,6 +104,8 @@ function saveReviewToDB()
         <main class="container flex-grow-1">
             <hr/>
             <?php
+            echo var_dump($_POST);
+            echo $movieID;
             if ($success)
             {
                 if ($intent == "posted")
